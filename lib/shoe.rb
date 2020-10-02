5d520bd874d724b7d155168249d81571da8ea743
Plimon Howard V <plimon.howard@gmail.com>
2020-10-02 21:14:10 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,42 @@
-# Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def brand
+    @brand
+  end
+
+  def color=(color)
+    @color = color
+  end
+  def color
+    @color
+  end
+
+  def size=(num)
+    @size = num
+  end
+  def size
+    @size
+  end
+
+  def material=(mat)
+    @material = mat
+  end
+  def material
+    @material
+  end
+
+  def condition=(con)
+    @condition = con
+  end
+  def condition
+    @condition
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
