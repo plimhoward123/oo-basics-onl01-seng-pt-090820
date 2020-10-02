46cf6cc943b289e0ae6b311d40649fa27e07196b
Plimon Howard V <plimon.howard@gmail.com>
2020-10-02 21:12:03 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,30 @@
-# Make your shoe class here!
+class Shoe
+  def initialize(brand)
+    @brand = brand
+  end
+
+  def brand
+    @brand
+  end
+
+  def color=(color)
+    @color = color
+  end
+  def color
+    @color
+  end
+
+  def size=(num)
+    @size = num
+  end
+  def size
+    @size
+  end
+
+  def material=(mat)
+    @material = mat
+  end
+  def material
+    @matertial
+  end
+end
